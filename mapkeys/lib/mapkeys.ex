abb3fba67f21d4d97518f742fcde83b94a881602
bukhashov <bukhashov2301@mail.ru>
2022-12-27 16:42:00 +0600

    start

---
+++ mapkeys/lib/mapkeys.ex
@@ -0,0 +1,18 @@
+defmodule Mapkeys do
+  @moduledoc """
+  Documentation for `Mapkeys`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Mapkeys.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ mapkeys/test/mapkeys_test.exs
@@ -0,0 +1,8 @@
+defmodule MapkeysTest do
+  use ExUnit.Case
+  doctest Mapkeys
+
+  test "greets the world" do
+    assert Mapkeys.hello() == :world
+  end
+end
